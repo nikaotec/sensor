b0ce2d500c48a44d76401953b7435e6027f275bf
Venancio <antoniovenancio10@gmail.com>
2026-03-22 15:45:35 -0300

    ajsutes

--- esp32/Config.h
+++ esp32/Config.h
@@ -12,7 +12,7 @@
 #define WIFI_SSID "VENANCIO"
 #define WIFI_PASS "liza1980"
 // #define MQTT_SERVER "173.249.10.19"
-#define MQTT_SERVER "109.123.240.215"
+#define MQTT_SERVER "nikaotech.com"
 #define MQTT_PORT 1883
 #define MQTT_USER ""
 #define MQTT_PASS ""
